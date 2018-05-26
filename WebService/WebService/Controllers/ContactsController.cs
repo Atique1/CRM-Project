9ed29c1a491a42cb2f731e699ab1a53a013251b2
Atique1 <atqueu@gmail.com>
2018-05-26 16:41:46 +0200

    first commit
    Added initial code

---
+++ WebService/WebService/Controllers/ContactsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.Models;
+using WebService.Processors;
+
+namespace WebService.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        [HttpPost]
+        [Route("SaveContact")]
+
+        // POST api/values
+        public bool SaveContact(Contact contact)
+        {
+            return ContactProcessor.ProcessContact(contact);
+        }
+
+        
+    }
+}
---
+++ WebService/WebService/Repositories/ContactRepositories.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebService.Models;
+
+namespace WebService.Repositories
+{
+    public class ContactRepositories
+    {
+        public static bool AddContactToDB(Contact contact)
+        {
+            var connectionString = "Data Source=.;Initial Catalog=ContactManager;User Id = sa; Password=12345";
+            var query = "INSERT INTO Contact(Name, PhoneNumber, Note) VALUES ('@Name','@PhoneNum','@Note');";
+
+            query = query.Replace("@Name", contact.Name).Replace("@PhoneNum", contact.PhoneNumber).Replace("@Note", contact.Note);
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                connection.Close();
+
+                return true;
+            }
+
+            catch(Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
